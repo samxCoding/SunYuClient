dde97fa6a19c97e55b231c54a9d6c793db277041
samxCoding <100733048+samxCoding@users.noreply.github.com>
2022-09-09 07:26:32 -0400

    Delete TFPanel.java

--- TFPanel.java
+++
@@ -1,7 +0,0 @@
-//welcome to the panel but with some ADMIN COMMANDS. This is for SunYuClient! Enable Admin Commands by setting the admincmds = true !
-admincmds = false
-  ==============
-  Admin Commands Location
-  /rebootfps = true
-  /rebootping = true
-  /matchmakingsver = true
