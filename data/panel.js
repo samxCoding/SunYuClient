7de3157bbddcd5cb3dad170efe5b9c9045575c93
samxCoding <100733048+samxCoding@users.noreply.github.com>
2022-09-09 07:29:35 -0400

    Create panel.js

---
+++ data/panel.js
@@ -0,0 +1,12 @@
+// console repeat: 
+("Hello! Starting new update for Online sunyu and moonyu")
+If no updates are required for UPDATE cancel process and let "user" carry on = true
+// If your internet went out maybe try using this panel
+internet_mode = true //Turn this off if your internet went out to play offline. If you try running MoonYU Client it wont work.
+updatenotify = true
+local_platforms = false
+custom_platformsmatchmaking = true("all") // put ur PREFERED platforms here to play if this is false, Matchmaking will be NORMAL MADE.
+localmodding = true
+-------------------
+  //admin panels
+  consolepanel = true | in ---> [game.exe> localfiles> consolesetting]
